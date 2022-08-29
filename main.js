fd3c33bebdd8ec12e72106211431b6ebcddb872f
Abigail <pineverdi@gmail.com>
2022-08-29 12:11:55 -0600

    Menu mobile

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+/* Menu Email */
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 menuEmail.addEventListener('click',aparecerMenu);
 
 function aparecerMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+/* Menu hamburguesa (movil) */
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+burgerMenu.addEventListener('click',aparecerMenuHamburguesa);
+
+function aparecerMenuHamburguesa(){
+    mobileMenu.classList.toggle('inactive');
 }
