3620821d18f14a0eeee2f984bc425587d25a51c2
Abigail <pineverdi@gmail.com>
2022-08-29 11:51:26 -0600

    Fusión del menú en desktop

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+menuEmail.addEventListener('click',aparecerMenu);
+
+function aparecerMenu(){
+    desktopMenu.classList.toggle('inactive');
+}
